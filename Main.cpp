3e947998c699ab5fd14e2db4e8bda78ba8dc0fb8
Parami Gunasekara <parami@live.ca>
2017-12-07 02:52:20 -0500

    created a Main file just to compile without errors

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Parami Gunasekara on 2017-12-06.
+//
+
+#include "Main.h"
+#include "RollOfDice.h"
+
+int main() {
+
+    return 0;
+}
---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Parami Gunasekara on 2017-12-06.
+//
+
+#ifndef QWINTO_QWIXX_BOARDGAME_MAIN_H
+#define QWINTO_QWIXX_BOARDGAME_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //QWINTO_QWIXX_BOARDGAME_MAIN_H
